805d396a067b60a6a1015cf86cdda9664c54ce9c
busrao51 <149954933+busrao51@users.noreply.github.com>
2023-11-26 12:59:45 +0300

    Add files via upload

---
+++ 7 HAFTA  ÖDEV.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int matris1[4][5] = { {1, 3, 5, 7, 9},
+{11, 13, 15, 17, 19},
+{2, 4, 6, 8, 10},
+{12, 14, 16, 18, 20}};
+int matris2[4][5] = { {1, 3, 5, 7, 9},
+{11, 13, 15, 17, 19},
+{2, 4, 6, 8, 10},
+{12, 14, 16, 18, 20}};
+bool durum = true;
+int i, j;
+for (i = 0; i < 4; i++)
+for (j = 0; j < 5; j++)
+if (matris1[i][j] != matris2[i][j])
+durum = false;
+if (durum)
+cout << "Sýralama ayný";
+else
+cout << "Sýralama farklý";
+return 0;
+}
